cde83bc94f4cd2d09f27c83b2b64c7c05fca1962
Cyrille Giquello <cyrille@giquello.fr>
2016-09-16 23:58:26 +0200

    add stuff

---
+++ Essais/essais05.js
@@ -0,0 +1,28 @@
+
+var dataEnMou = {
+	
+	"type": "FeatureCollection",
+	"features": [
+	{
+		"geometry": {
+			"type": "Point",
+			"coordinates": [0.99, 48.5]
+		},
+		"type": "Feature",
+		"properties": {
+			"popupContent": "Point mou 1"
+		}
+	},
+	{
+		"geometry": {
+			"type": "Point",
+			"coordinates": [0.99, 48]
+		},
+		"type": "Feature",
+		"properties": {
+			"popupContent": "Point mou 2"
+		}
+	}
+	]
+};
+
